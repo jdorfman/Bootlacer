94451c5b83f378e6cba191d266e5f8c85bddbe46
root <root@testdev.dibblrmedia.com>
2012-09-11 14:23:04 -0400

    Changed some stuff on the barebones page and repushing script

--- bootlacer.sh
+++ bootlacer.sh
@@ -7,12 +7,13 @@
 #  
 #  Script will download latest bootstrap version, create a project folder, and give you options to
 #  create a base template and add a range of CDN script libraries into your page automatically. 
-#  Again.... I am lazy.
+#
+#  Again.... I am lazy and got sick of doing this.
 # 
 #  USAGE
 #     -Set your directory below where you want all projects created under. Typical WWW root.
-#     -Run with minium requirements: sh bootlacer.sh <projectname>
-#     -Project folder will be created for you and bootstrap will be enabled there.
+#     -Run script with your project name after it. Example: sh bootlacer.sh mynewproject
+#     -The "mynewproject" folder will be created for you. You will then be able to add CDN libraries to your page.
 #
 #  ADDITIONAL LIBRARIES
 #     -JqueryUI		http://jqueryui.com
@@ -22,11 +23,6 @@
 #     -Modernizr	http://modernizr.com/
 #     -Init.js		Quick document.ready() inclusion for quick jQuery scripting
 #	 
-#  SUPPORTED TEMPLATES
-#     -Very Basic       Minimum HTML tags for page
-#     -Hero 		http://twitter.github.com/bootstrap/examples/hero.html
-#     -Fluid		http://twitter.github.com/bootstrap/examples/fluid.html
-#     -Starter		http://twitter.github.com/bootstrap/examples/starter-template.html
 #
 #
 # CHANGE THIS IF YOU WANT TO INSTALL FILES TO OTHER ROOT FOLDER THAN DEFAULT HTTP FOLDER
@@ -45,79 +41,64 @@ fi
 echo "\r\n\r\n"
 echo "Fetching most recent Boostrap version from da GitHuz"
 wget -O /tmp/bootstrap.zip http://twitter.github.com/bootstrap/assets/bootstrap.zip
-#
-if [ -f /tmp/bootstrap.zip ]
-then
-#
 unzip /tmp/bootstrap.zip
 mkdir $PROJ_DIR$1
 echo "Creating project: " $1
 echo "Loading boostrap"
 mv bootstrap/* $PROJ_DIR$1
 rm -fR bootstrap/
-echo "Basics are done for bootstrap. \r\nCreat a page template??\r\n1) Add Hero Template\r\n2)Add Fluid\r\n3)Add Starter\r\n4) Add bare bones?\r\n5) Exit"
-read reponse
-if ( "$response" == "1" )
- echo "Getting Hero..."
- wget http://twitter.github.com/bootstrap/examples/hero.html
- mv hero.html $PROJ_DIR$1
- break
-elif ( "$response" == "2" )
- echo "Getting Hero..."
- wget http://twitter.github.com/bootstrap/examples/fluid.html
- mv hero.html $PROJ_DIR$1
- break
-elif ( "$response" == "3" )
- echo "Getting Hero..."
- wget http://twitter.github.com/bootstrap/examples/starter-template.html
- mv hero.html $PROJ_DIR$1
- break
-elif ( "$response" == "4" )
- echo "Writing bare bones template..."
- wget http://.html
- mv hero.html $PROJ_DIR$1
- break
-elif ( "$response" == "5" )
- exit 1
+
+read -p "Create barebones Bootstrap page?(y/n)?"
+if [ $REPLY == "y" ]; then
+	wget https://raw.github.com/boxxa/Bootlacer/master/barebones.html
 else
- exit 1
+	echo "All done."
+	exit 1
 fi
 
-echo "Add any additional libararies to your page?"
-read -p "Add any additional libararies to your page?(y/n)?"
-if [ $REPLY != "y" ]; then
-	echo "Exiting..."
+read -p "Do you want to start adding libraries to your page? (y/n)"
+if [ $REPLY == "n" ]; then
+	echo "  </body>" >> barebones.html
+	echo "</html>" >> barebones.html
+	mv barebones.html $PROJ_DIR$1"/index.html"
+	echo "Created " $PROJ_DIR$1
 	exit 1
 fi
 read -p "Add jQueryUI support? (y/n)"
 if [ $REPLY == "y" ]; then
-        echo "Added jQueryUI to " response
-        break
+        echo "   <script src='//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js'></script>" >> barebones.html
+	echo "Adding jQueryUI"
 fi
 read -p "Add Prototype support?(y/n)"
 if [ $REPLY == "y" ]; then
-        echo "Added jQueryUI to " response
-        break
+       echo "    <script src='//ajax.googleapis.com/ajax/libs/prototype/1.7.1.0/prototype.js'></script>" >> barebones.html
+	echo "Adding Prototype"
 fi
 read -p "Add Chromeframe support? (y/n)"
 if [ $REPLY == "y" ]; then
-        echo "Added jQueryUI to " response
-        break
+        echo "   <script src='//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js'></script>" >> barebones.html
+	echo "Adding Chromeframe" 
 fi
 read -p "Add AngularJS support? (y/n)"
 if [ $REPLY == "y" ]; then
-        echo "Added jQueryUI to " response
-        break
+        echo "   <script src='//ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min.js'></script>" >> barebones.html
+	echo "Adding AngularJS"
 fi
 read -p "Add Modernizr support? (y/n)"
 if [ $REPLY == "y" ]; then
-        echo "Added jQueryUI to " response
-        break
+        echo "   <script src='//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.1/modernizr.min.js'></script>" >> barebones.html 
+	echo "Adding Modernizr"
 fi
-
-
-
-
+read -p "Add an init.js script for your jQuery data? (y/n)"
+if [ $REPLY == "y" ]; then
+        echo "   <script src='init.js'><script>" >> barebones.html
+	touch $PROJ_DIR$1/init.js
+	echo "Created init.js"
+fi
+echo "  </body>" >> barebones.html
+echo "</html>" >> barebones.html
+mv barebones.html $PROJ_DIR$1"/index.html"
+echo "Created " $PROJ_DIR$1
 #
 if [ -z "$2" ]
         then
@@ -125,8 +106,4 @@ if [ -z "$2" ]
          exit 1
 fi
 #
-else
- echo "Not able to get bootstrap file. Online?"
- exit 1
-fi
 exit 1
